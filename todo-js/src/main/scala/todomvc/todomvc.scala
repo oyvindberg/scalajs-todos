c31e3831c20e47d2d12fcc317b487c2e7580140c
Øyvind Raddum Berg <elacin@gmail.com>
2015-06-25 18:36:12 -0500

    package object comment

--- todo-js/src/main/scala/todomvc/todomvc.scala
+++ todo-js/src/main/scala/todomvc/todomvc.scala
@@ -3,6 +3,7 @@ import japgolly.scalajs.react.vdom._
 package object todomvc extends Base {
   import scalacss.ScalaCssReact._
 
+  /* shows how to create new, alreayd styled html elements */
   object < extends JustTags {
     val myButton = button(CommonStyle.button)
   }
