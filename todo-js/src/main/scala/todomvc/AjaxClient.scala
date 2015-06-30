e2875e06ca1368f2f3360ec092cb980fca7f4334
Øyvind Raddum Berg <elacin@gmail.com>
2015-06-30 15:32:06 -0500

    use Ajax from scalajs-dom to make calls

--- todo-js/src/main/scala/todomvc/AjaxClient.scala
+++ todo-js/src/main/scala/todomvc/AjaxClient.scala
@@ -1,42 +1,14 @@
 package todomvc
 
-import org.scalajs.dom
-import org.scalajs.dom.XMLHttpRequest
-import org.scalajs.dom.ext.AjaxException
+import org.scalajs.dom.ext.Ajax
 import upickle._
 
-import scala.concurrent.{Future, Promise}
+import scala.concurrent.Future
 
 object AjaxClient extends autowire.Client[String, Reader, Writer] {
 
-  def post(url:             String,
-           data:            String,
-           timeout:         Int                 = 0,
-           headers:         Map[String, String] = Map.empty,
-           withCredentials: Boolean             = false,
-           responseType:    String              = ""): Future[XMLHttpRequest] = {
-    val req     = new dom.XMLHttpRequest()
-    val promise = Promise[dom.XMLHttpRequest]()
-
-    req.onreadystatechange = { (e: dom.Event) =>
-      if (req.readyState == 4) {
-        if ((req.status >= 200 && req.status < 300) || req.status == 304)
-          promise.success(req)
-        else
-          promise.failure(AjaxException(req))
-      }
-    }
-    req.open("POST", url)
-    req.responseType = responseType
-    req.timeout = timeout
-    req.withCredentials = withCredentials
-    headers.foreach(x => req.setRequestHeader(x._1, x._2))
-    req.send(data)
-    promise.future
-  }
-
   override def doCall(req: Request): Future[String] =
-    post(
+    Ajax.post(
       url          = s"todo-api/${req.path.mkString("/") }",
       data         = write(req.args),
       timeout      = 4000
